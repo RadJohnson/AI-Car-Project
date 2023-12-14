26a6a5de286f5fa85e76a9308c0a8549b0f50c2e
RadJohnson <radjohnson77@gmail.com>
2023-12-14 11:17:15 +0000

    Chore: revised the manages code

--- LEARNING AI PROJECT/Assets/Scripts/AIManager.cs
+++ LEARNING AI PROJECT/Assets/Scripts/AIManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.IO;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AIManager : MonoBehaviour
@@ -40,52 +41,33 @@ public class AIManager : MonoBehaviour
                 //This may need to change so that the AI get given more score based on number of checkpoints passed through
                 for (int i = 0; i < populationSize; i++)
                 {
-                    //seems like it should now be working
-
-
                     if (!agentList[i].hascrashed)
                         nets[i].AddFitness((float)(agentList[i].checkpointsPassedThrough + (float)CumulativeCheckpointDistance(agentList[i].checkpointsPassedThrough)));
-                    else 
-                        nets[i].AddFitness(0);
+                    else
+                        nets[i].SetFitness(nets[i].GetFitness() - 10);
 
-                    //nets[i].AddFitness(CumulativeCheckpointDistance(agentList[i].checkpointsPassedThrough));
-
-                    //nets[i].AddFitness((float)agentList[i].checkpointsPassedThrough +
-                    //    -(agentList[i].gameObject.transform.position - checkpoints[agentList[i].checkpointsPassedThrough + 1].transform.position).magnitude);
-                    //Want to probably add a bit for distance to the next checkpoint and from the most recently passed through for better granularity
-
-                    //nets[i].AddFitness((agentList[i].transform.position - startPoint.position /*+ agentList[i].transform.position + endpoint.position*/).magnitude);
                 }
 
                 nets.Sort();//puts the fitest at the bottom of this which is why next 2 for loops are the way they are
-                //nets.Reverse();//not needed unless I want it to become worse overtime
+                nets.Reverse();
 
 
-                for (int i = populationSize - 1, agentNum = 0; i > populationSize - 11; i--, agentNum++)//may want to only save the first few networks rather than all of them
-                //for (int i = 0; i < 10; i++)//may want to only save the first few networks rather than all of them
+                for (int i = 0; i < populationSize; i++)//may want to only save the first few networks rather than all of them
                 {
                     var contentToSave = JsonUtility.ToJson(nets[i], true);
-                    string filePath = Path.Combine(Application.persistentDataPath, $"Generation_{generationNumber} NeuralNetwork_{agentNum}.json");//want to now change the numbers in the file name so it is more logical when lookign at the save files
+                    string filePath = Path.Combine(Application.persistentDataPath, $"Generation_{generationNumber} NeuralNetwork_{/*agentNum*/i}.json");//want to now change the numbers in the file name so it is more logical when lookign at the save files
                     //change application.persistant datapath to a coppied file path since it is currently saving to appdata\locallow\(unity project counter)
                     //Debug.Log(filePath);
 
                     File.WriteAllText(filePath, contentToSave);
                 }
 
-                /*
-                for (int i = 0; i < populationSize; i++)//this if I have time will be chaged so that the second half of the agents get the previous version coppied onto them
+                for (int i = 0; i < populationSize / 2; i++)
                 {
+                    // here is an issue as i am coppying the worst over to the front
+                    nets[i + populationSize / 2] = new AIBrain(nets[i]);
                     nets[i] = new AIBrain(nets[i]);
                     nets[i].MutateWeights();
-                }
-                */
-
-
-                for (int i = 0; i < populationSize / 2; i++)//this if I have time will be chaged so that the second half of the agents get the previous version coppied onto them
-                {
-                    nets[i] = new AIBrain(nets[i + (populationSize / 2)]);
-                    nets[i].MutateWeights();
-                    nets[i + (populationSize / 2)] = new AIBrain(nets[i + (populationSize / 2)]);
                 }
 
 
@@ -143,14 +125,15 @@ public class AIManager : MonoBehaviour
             }
         }
         else
-        {// might want to save all of a generation and put them all in again on their own for learning continuation
+        {
+            //modify so it loads all of a generation back in
             string brain = File.ReadAllText(brainFilePath);
 
             generationNumber = JsonUtility.FromJson<AIBrain>(brain).generation;
 
             for (int i = 0; i < populationSize; i++)
             {
-                AIBrain net = new AIBrain(JsonUtility.FromJson<AIBrain>(brain));
+                AIBrain net = new AIBrain(JsonUtility.FromJson<AIBrain>(brain));// this might not be working correctly
                 nets.Add(net);
             }
         }
